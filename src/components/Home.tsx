23e0d32a67486a606230d243160c870dff56b445
mdreyadhossain <reyadhosen@gmail.com>
2025-12-09 19:32:46 +0600

    home ui has been updated

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { AddIcon, Box, Button, Divider, TextField } from "convertupleads-theme";
+import { AddIcon, Box, Button, Divider, FormLabel, TextField } from "convertupleads-theme";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 // import { v4 as uuidV4 } from "uuid";
@@ -18,29 +18,41 @@ export default function Home() {
     };
 
     return (
-        <Box sx={{ padding: 4, height: "80vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-            <Box>
-                <h1>Discushy</h1>
-                <p>Start or join a discussion room</p>
-                <Button onClick={createRoom} startIcon={<AddIcon />}>Create New Room</Button>
-            </Box>
+        <>
+            <Box sx={{
+                padding: 4,
+                height: "80vh",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: 6
+            }}>
+                <Box>
+                    <h1>Discushy</h1>
+                    <p>Start or join a discussion room</p>
+                    <Button onClick={createRoom} startIcon={<AddIcon />}>Create New Room</Button>
+                </Box>
 
-            <Box sx={{ marginTop: 4 }}>
-                <TextField
-                    placeholder="Enter Room ID"
-                    value={roomId}
-                    onChange={(e) => setRoomId(e.target.value)}
-                    autoComplete="off"
-                />
-                <br />
-                <br />
-                <Button variant="tonal" onClick={joinRoom}>Join Room</Button>
-            </Box>
+                <Box sx={{ marginTop: 4, border: '1px solid #ccc', borderRadius: 2, padding: 2 }}>
+                    <FormLabel>Join a Room</FormLabel>
+                    <br />
+                    <TextField
+                        placeholder="Enter Room ID"
+                        value={roomId}
+                        onChange={(e) => setRoomId(e.target.value)}
+                        autoComplete="off"
+                    />
+                    <br />
+                    <br />
+                    <Button variant="tonal" onClick={joinRoom}>Join Room</Button>
+                </Box>
 
+            </Box>
             <Box sx={{ position: 'absolute', bottom: 16, width: '100%', textAlign: 'center' }}>
                 <Divider />
                 <p>Developed By <a href="https://beetcoder.com" target="_blank">BeetCoder</a></p>
             </Box>
-        </Box>
+        </>
     );
 }
