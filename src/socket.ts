2b5e92104090bfcb307d88e7a59c638c132ee752
mdreyadhossain <reyadhosen@gmail.com>
2025-12-09 17:14:01 +0600

    backend production server added

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3100";
+// const URL = "http://localhost:3100";
+const URL = "https://ai-interview-3o-be.onrender.com";
 
 export const socket = io(URL);
