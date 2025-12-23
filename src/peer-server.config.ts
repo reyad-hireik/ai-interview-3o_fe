8072152dc698dffdd097ff035aa80e638e43a009
MdReyadHossain <reyadhosen@gmail.com>
2025-12-24 01:11:11 +0600

    peer server configuration has been implemented

--- src/peer-server.config.ts
+++ src/peer-server.config.ts
@@ -2,36 +2,14 @@ export const peerServerTest = {
     host: 'peer-server-rn7x.onrender.com',
     port: 443,
     secure: true,
-    path: '/peerjs',
-    config: {
-        iceServers: [
-            { urls: 'stun:stun.cloudflare.com:3478' },
-            { urls: 'stun:stun.cloudflare.com:53' },
-            {
-                urls: 'turn:turn.cloudflare.com:3478',
-                username: 'g073965b8a81b91b99efc53dae7d5617a16e34931b8ecf107b6cb3fc4141ca0a',
-                credential: 'ba3944495053f1856e566d33123d2a34aeb7b366f5f2173391c88373b877cd23'
-            }
-        ]
-    }
+    path: '/peerjs'
 }
 
 export const peerServerGlobal = {
     host: '0.peerjs.com',
     port: 443,
     secure: true,
-    path: '/',
-    config: {
-        iceServers: [
-            { urls: 'stun:stun.l.google.com:19302' },
-            { urls: 'stun:stun1.l.google.com:19302' },
-            {
-                urls: 'turn:openrelay.metered.ca:80',
-                username: 'openrelayproject',
-                credential: 'openrelayproject'
-            }
-        ]
-    }
+    path: '/'
 }
 
 export const peerServerBT1 = {
@@ -39,6 +17,9 @@ export const peerServerBT1 = {
     port: 443,
     secure: true,
     path: '/peerjs',
+};
+
+export const peerIceServerCloudFlare = {
     config: {
         iceServers: [
             {
@@ -61,4 +42,34 @@ export const peerServerBT1 = {
             }
         ]
     }
-};
+}
+
+export const peerIceServerGlobal = {
+    config: {
+        iceServers: [
+            { urls: 'stun:stun.l.google.com:19302' },
+            { urls: 'stun:stun1.l.google.com:19302' },
+            {
+                urls: 'turn:openrelay.metered.ca:80',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+            }
+        ]
+    }
+}
+
+export const peerIceServerCloudFlare2 = {
+    config: {
+        iceServers: [
+            { urls: 'stun:stun.cloudflare.com:3478' },
+            { urls: 'stun:stun.cloudflare.com:53' },
+            {
+                urls: 'turn:turn.cloudflare.com:3478',
+                username: 'g073965b8a81b91b99efc53dae7d5617a16e34931b8ecf107b6cb3fc4141ca0a',
+                credential: 'ba3944495053f1856e566d33123d2a34aeb7b366f5f2173391c88373b877cd23'
+            }
+        ]
+    }
+}
+
+export const peerConfiguration = { ...peerServerBT1, ...peerIceServerCloudFlare };
